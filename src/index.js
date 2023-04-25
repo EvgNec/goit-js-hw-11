397426b956aac0e3aa1cefc219b58ad065986e00
Evgeniy <evgnec@gmail.com>
2023-04-25 15:06:49 +0300

    add

---
+++ src/index.js
@@ -0,0 +1,86 @@
+import ApiService from "./js/apiService"
+import renderService from "./js/renderService"
+import Notiflix from "notiflix"
+import { debounce } from "lodash"
+
+
+
+const api = new ApiService()
+const renderMaker = new renderService()
+
+const refs = {
+    form: document.querySelector('.search-form'),
+    // loadMoreBtn: document.querySelector('.load-more'),
+    galleryContainer: document.querySelector('.gallery'),
+    fetchDataBtn: document.querySelector('[data-load="getData"]'),
+    loader: document.querySelector('[data-loader]'),
+}
+
+refs.form.addEventListener('submit', renderImage)
+// refs.loadMoreBtn.addEventListener('click', loadMore)
+
+
+function  onScroll() {
+  const height = document.body.offsetHeight
+  const screenHeight = window.innerHeight
+
+  const scrolled = window.scrollY
+
+  const threshold = height - screenHeight / 4
+
+  const position = scrolled + screenHeight
+
+    if (position >= threshold) {
+      loadMore()
+  }
+}
+
+function renderImage(e) {
+
+    e.preventDefault()
+
+    api.resetPage()
+
+    api.searchQuery = e.currentTarget.elements.searchQuery.value.trim()
+
+    if (api.searchQuery === '') {
+        return Notiflix.Notify.warning('please enter more query to find!')
+    }
+
+    renderMaker.clearGallery()
+    toggleLoader()
+    api.fetchImages()
+        .then(data => {
+            if (data.totalHits === 0) {
+                return Notiflix.Notify.info('Sorry, there are no images matching your search query. Please try again.')
+            }
+            Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`)
+            toggleLoader()
+            renderMaker.renderImages(data.hits)
+            api.incrementPage()
+
+            // renderMaker.showLoadBtn()
+            window.addEventListener('scroll', debounce(onScroll, 500))
+        })
+    }
+
+function loadMore() {
+
+    if (api.totalHits < 0) {
+        // renderMaker.hideLoadBtn()
+        return Notiflix.Notify.info('We&#x60;re sorry, but you&#x60;ve reached the end of search results.')
+    }
+    // renderMaker.hideLoadBtn()
+        toggleLoader()
+        api.fetchImages().then(data => {
+            toggleLoader()
+        renderMaker.renderImages(data.hits)
+        api.incrementPage()
+    })
+}
+
+function toggleLoader() {
+    refs.loader.classList.toggle('is-hidden')
+}
+
+
---
+++ src/js/apiService.js
@@ -0,0 +1,61 @@
+import axios from "axios";
+import { reject } from "lodash";
+import Notiflix from "notiflix";
+
+const instance = axios.create({
+        baseURL: 'https://pixabay.com/api/',
+        headers: {'Content-Type': 'application/json'}
+    })
+
+
+export default class ApiService {
+
+    _apiKey = '34332611-52435d07ae30b60a07e51363a';
+
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+        this.per_page = 40;
+        this.totalHits = 0;
+    }
+
+    get query() {
+        return this.searchQuery;
+    }
+
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+
+    get hits() {
+        return this.totalHits;
+    }
+
+    set hits(newValue) {
+        this.totalHits = newValue
+    }
+
+    incrementPage() {
+        this.page += 1;
+    }
+
+    resetPage() {
+        this.page = 1;
+    }
+
+    decrementTotalHits() {
+        this.totalHits -= (this.page * this.per_page)
+    }
+
+    async fetchImages() {
+        console.log(this)
+        return await instance.get(`?key=${this._apiKey}&q=${this.searchQuery}&page=${this.page}&per_page=${this.per_page}&image_type=photo&orientation=horizontal&safesearch=true`)
+            .then(response => {
+                this.hits = response.data.totalHits
+                this.decrementTotalHits()
+                return response.data
+            }
+        )
+            .catch(error => Notiflix.Notify.warning(error))
+    }
+}
